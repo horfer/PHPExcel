bed551152eea0c25e8abc1f2e0dbd8fae41229c3
Sandor Zemko <sandor.zemko@nexum.hu>
2022-06-02 10:08:49 +0200

    error style fix

--- Classes/PHPExcel/Writer/Excel5/Worksheet.php
+++ Classes/PHPExcel/Writer/Excel5/Worksheet.php
@@ -2827,16 +2827,17 @@ class PHPExcel_Writer_Excel5_Worksheet extends PHPExcel_Writer_Excel5_BIFFwriter
                 // error style
                 $errorStyle = $dataValidation->getType();
                 switch ($errorStyle) {
-                    case PHPExcel_Cell_DataValidation::STYLE_STOP:
-                        $errorStyle = 0x00;
-                        break;
                     case PHPExcel_Cell_DataValidation::STYLE_WARNING:
                         $errorStyle = 0x01;
                         break;
                     case PHPExcel_Cell_DataValidation::STYLE_INFORMATION:
                         $errorStyle = 0x02;
                         break;
+                    default:
+                        $errorStyle = 0x00;
+                        break;
                 }
+
                 $options |= $errorStyle << 4;
 
                 // explicit formula?
